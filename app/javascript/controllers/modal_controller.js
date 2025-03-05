ad2f0bbd4b83089d8d33d53784a9bc86dfb900d5
Silver Luhtoja <silver.luhtoja@gmail.com>
2025-03-05 23:05:29 +0200

    working on product modal and options selection

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,11 +2,30 @@ import { Controller } from '@hotwired/stimulus';
 
 // Connects to data-controller="modal"
 export default class extends Controller {
+  static targets = ['option', 'overlay'];
   // initial when modal opens
-  // connect() {
-  // console.log('HEllo from modal controller');
-  // console.log(this.element);
-  // }
+  connect() {
+    this.setInitialSelection();
+  }
+
+  setInitialSelection() {
+    if (this.optionTargets.length > 0) {
+      this.optionTargets[0].classList.add('selected');
+      this.overlayTargets[0].classList.add('show');
+    }
+  }
+
+  optionPicked(event) {
+    // Remove 'selected' class from all options
+    this.optionTargets.forEach(option => option.classList.remove('selected'));
+
+    // Add 'selected' class to the clicked option
+    event.currentTarget.classList.add('selected');
+
+    const newImage = event.currentTarget.querySelector('img').outerHTML;
+    this.overlayTarget.innerHTML = newImage;
+    this.overlayTarget.classList.add('show');
+  }
 
   close() {
     this.element.remove();
